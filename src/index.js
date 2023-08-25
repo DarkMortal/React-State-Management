7d63c735258440d2247785c02c818e9de5a55700
Saptarshi Dey <dragonbeast.saiyan@gmail.com>
2023-08-25 14:14:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import App from "./App";
+
+String.prototype.strip = function () {
+  return this.replace(/^\s+/, "").replace(/\s+$/, "");
+};
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
